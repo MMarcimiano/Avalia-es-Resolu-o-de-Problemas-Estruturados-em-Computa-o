869522b4e134d92e31f526470299134b1d78680a
Mateus Henrique Marcimiano Batista <107581390+MMarcimiano@users.noreply.github.com>
2023-11-13 17:05:57 -0300

    Update HashMapFNV1.java

--- RA3 Hash/TrabalhoHash/src/HashMapFNV1.java
+++ RA3 Hash/TrabalhoHash/src/HashMapFNV1.java
@@ -11,9 +11,9 @@ public class HashMapFNV1 extends HashMap {
     @Override
     public int hash(int value) {
         final int FNV_PRIME = 16777619;
-        final long FNV_OFFSET_BASIS = 2166136261L; // Correção: Use long em vez de int
+        final long FNV_OFFSET_BASIS = 2166136261L; // Use long em vez de int
         
-        int hash = (int) FNV_OFFSET_BASIS; // Converta FNV_OFFSET_BASIS de long para int
+        int hash = (int) FNV_OFFSET_BASIS; // Converter de long para int
         
         while (value != 0) {
             hash ^= (value & 0xFF);
